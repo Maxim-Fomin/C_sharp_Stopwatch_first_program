68097302bf50e72f91b8fa1eb9619dd7962dc260
Maxim Fomin <fomin.maxsim@gmail.com>
2020-11-28 15:45:36 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace MyStopwatchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (int i = 0; i < 3; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration: " + stopwatch.GetInterval());
+
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyStopwatchTest
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private bool _isRanning;
+
+        public void Start()
+        {
+            if (_isRanning)
+            {
+                throw new InvalidOperationException("Stopwatch is already running!");
+            }
+
+            _startTime = DateTime.Now;
+            _isRanning = true;
+ 
+        }
+
+        public void Stop()
+        {
+            if(!_isRanning)
+            {
+                throw new InvalidOperationException("Stopwatch is not running!");
+            }
+
+            _endTime = DateTime.Now;
+            _isRanning = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;     
+        }
+    }
+}
